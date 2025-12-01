ce30cb7a775ab4901baa9637b735b3d45e28cef6
SolomonJSX <janserik405@outlook.com>
2025-12-01 23:05:16 +0500

    GetTotalAmount was released

--- ECommerceApp.Application/Services/Implementations/Cart/CartService.cs
+++ ECommerceApp.Application/Services/Implementations/Cart/CartService.cs
@@ -2,12 +2,14 @@
 using ECommerceApp.Application.DTOs;
 using ECommerceApp.Application.DTOs.Cart;
 using ECommerceApp.Application.Services.Interfaces.Cart;
+using ECommerceApp.Domain.Entities;
 using ECommerceApp.Domain.Entities.Cart;
+using ECommerceApp.Domain.Interfaces;
 using ECommerceApp.Domain.Interfaces.Cart;
 
 namespace ECommerceApp.Application.Services.Implementations.Cart;
 
-public class CartServic(ICart cartEntity, IMapper mapper) : ICartService
+public class CartServic(ICart cartEntity, IMapper mapper, IGeneric<Product> productRepository) : ICartService
 {
     public async Task<ServiceResponse> SaveCheckoutHistories(IEnumerable<CreateAchieve> checkouts)
     {
@@ -18,8 +20,28 @@ public class CartServic(ICart cartEntity, IMapper mapper) : ICartService
         return result > 0 ? new ServiceResponse(true, "Checkout Achieved") : new ServiceResponse(false, "Checkout Failed");
     }
 
-    public Task<ServiceResponse> Checkout(Checkout checkout, string userId)
+    public async Task<ServiceResponse> Checkout(Checkout checkout, string userId)
     {
         throw new NotImplementedException();
     }
+
+    public async Task<(IEnumerable<Product>, decimal)> GetTotalAmount(IEnumerable<ProcessCart> processCarts)
+    {
+        if (!processCarts.Any()) return ([], 0);
+
+        var products = await productRepository.GetAllAsync();
+
+        if (!products.Any()) return ([], 0);
+
+        var carts = processCarts
+            .Select(cartItem => products.FirstOrDefault(p => p.Id == cartItem.ProductId))
+            .Where(p => p != null)
+            .ToList();
+
+        var totalAmount = processCarts
+            .Where(cartItem => products.Any(p => p.Id == cartItem.ProductId))
+            .Sum(cartItem => cartItem.Quantity * carts.First(p => p.Id == cartItem.ProductId).Price);
+        
+        return (carts, totalAmount);
+    } 
 }
